df0495d5d31396b084ca281b5a9306085e71cf72
Fumiya Katoh <katoh.numazu.ct@gmail.com>
2015-01-19 23:59:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### DXライブラリ C#サンプル ###
+
+DxLibを使ってFPS制御するひな形（[leaptheremin](https://africa.denki.numazu-ct.ac.jp/gitbucket/kosensai2014/leaptheremin)で実際に使ったものを適当に使えるようにした）もの。
+
+GameLoop.csをベースに開発していく形となります。
+
+### 参考 ###
+
+[ＤＸライブラリ置き場](http://homepage2.nifty.com/natupaji/DxLib/)
---
+++ nesemu/GameLoop.cs
@@ -0,0 +1,52 @@
+﻿using DxLibDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nesemu {
+	/// <summary>
+	/// ゲーム本体の制御を行うクラスです
+	/// </summary>
+	class GameLoop {
+		//DxLibのデフォルトスクリーンサイズは640,480 (DX.SetGraphModeで変更可能
+		const int center_x = 320;
+		const int center_y = 240;
+
+		double radius = 0;
+		const int radius_max = 200;
+
+		/// <summary>
+		/// ゲーム起動時に一度だけ呼び出されます
+		/// </summary>
+		public void Setup() {
+		}
+		/// <summary>
+		/// 敵の移動など計算を行います
+		/// </summary>
+		public void Calculate() {
+			radius = radius_max * Math.Cos(DX.GetNowCount() / 1000.0);//円の半径を変更
+		}
+		/// <summary>
+		/// 描画処理を行います
+		/// </summary>
+		public void Update() {
+			DX.DrawCircle(center_x, center_y, (int)radius, DX.GetColor(0, 0, 255));//青色の円を描画
+			DX.DrawString(center_x, center_y, "Hello DxLib.", DX.GetColor(0, 255, 0));
+		}
+		/// <summary>
+		/// ゲーム終了時に呼び出されます。
+		/// </summary>
+		public void Exit() {
+		}
+		/// <summary>
+		/// ゲームを終了するかどうかの真偽値を返します
+		/// </summary>
+		/// <returns>ゲームを終了する場合はtrue</returns>
+		public bool IsEnd() {
+			return DX.CheckHitKey(DX.KEY_INPUT_ESCAPE) == 1;//ESCキーが押されたら終了する
+		}
+	}
+}
